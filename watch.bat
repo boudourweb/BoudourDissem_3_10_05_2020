cfef533d6d3bdf080172269816d2cdad01de2440
Boudour Dissem <hachani.boudour@hotmail.fr>
2021-07-07 00:16:28 +0200

    Responsive design

--- watch.bat
+++ watch.bat
@@ -1,3 +1,3 @@
 ./dart-sass/sass sass/style.scss style.css --watch
-./dart-sass/sass sass/style_a_la_francaise.scss style_a_la_francaise.css --watch
+./dart-sass/sass sass/style_restaurant.scss style_restaurant.css --watch
 ./dart-sass/sass sass/loader.scss loader.css --watch
